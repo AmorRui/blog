a50cddd59237279833187fd068fc03cad97dadc3
Amor <teng0009@126.com>
2018-02-13 21:29:38 +0800

    mysql

--- blog.sql
+++ blog.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50637
 File Encoding         : 65001
 
-Date: 2018-02-13 21:17:39
+Date: 2018-02-13 21:26:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -57,56 +57,3 @@ INSERT INTO `article` VALUES ('144', 'git 基础使用总结（2）----分支与
 INSERT INTO `article` VALUES ('145', 'git 基础使用总结（1）--基础应用', 'live like youself.写在前面git和github并不能混为一谈1.git 可以在 .git 文件夹里面维护你的历史代码2.github是用Git做版本控制的代码托管平台，每家公司或者包括你自己都可以有一个自己的代码托管平台3.使用代码托管平台时，需要上传本地的公钥具体教程使用git前应输入此命令配置git config --global ...', '\n\n				<blockquote>\n  <p>live like youself.</p>\n</blockquote>\n\n<h1 id=\"&#x5199;&#x5728;&#x524D;&#x9762;\">&#x5199;&#x5728;&#x524D;&#x9762;</h1>\n\n<p>git&#x548C;github&#x5E76;&#x4E0D;&#x80FD;&#x6DF7;&#x4E3A;&#x4E00;&#x8C08;</p>\n\n<p>1.git &#x53EF;&#x4EE5;&#x57', '/uploads\\avater.jpg', '2018', '2', '12');
 INSERT INTO `article` VALUES ('146', 'string.prototype常用方法中slice substr substring', 'live like youself.str.substr(start[, length])提取字符串的一部分，不改变原字符串参数1.start :开始提取字符串的位置，若为负数，则重字符串末尾开始，str.substr(str.length-1) 等价于str.substr(-1);2.Length :可选参数，截取字符串的长度，如不填则默认截取到字符串末尾注...', '\n\n				<blockquote>\n  <p>live like youself.</p>\n</blockquote>\n\n<h2 id=\"strsubstrstart-length\">str.substr(start[, length])</h2>\n\n<p>&#x63D0;&#x53D6;&#x5B57;&#x7B26;&#x4E32;&#x7684;&#x4E00;&#x90E8;&#x5206;&#xFF0C;&#x4E0D;&#x6539;&#x53D8;&#x539F;&#x5B57;&#x7B', '/uploads\\avater.jpg', '2018', '2', '12');
 INSERT INTO `article` VALUES ('147', 'SASS入门学习总结', 'live like youself.SASS是一种css开发工具，SASS使用Ruby语言写的基本用法变量SASS允许使用变量，所有变量用$开头例如：　　$blue : #1875e7;　　　div {　　　color : $blue;　　}如果变量需要镶嵌到字符串之中，就必须写在#{}之中。例如　　$side : left;...', '\n\n				<blockquote>\n  <p>live like youself.</p>\n</blockquote>\n\n<p>SASS&#x662F;&#x4E00;&#x79CD;css&#x5F00;&#x53D1;&#x5DE5;&#x5177;&#xFF0C;SASS&#x4F7F;&#x7528;Ruby&#x8BED;&#x8A00;&#x5199;&#x7684;</p>\n\n<hr>\n\n<h1 id=\"&#x57FA;&#x672C;&#x7528;&#x6CD5;\">&#x57FA;&', '/uploads\\avater.jpg', '2018', '2', '12');
-
--- ----------------------------
--- Table structure for `posts`
--- ----------------------------
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `brief` varchar(255) DEFAULT NULL,
-  `content` text,
-  `status` tinyint(4) DEFAULT NULL,
-  `time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of posts
--- ----------------------------
-INSERT INTO `posts` VALUES ('1', '1', 'JS the Hardcore: 执行上下文(Execution Context)', '先看个例子: console.log(a) console.log(foo()) var a = \'hello world\' function foo() { console.log(\'foo\') } 估计大部分人早就对这种问题了如指掌了，输出结果也是脱口而出', '<p>这几年，前端技术发展很快，每年都有新的技术名词流行，ReactJS、Angular 2、VUE、微信小程序。。。而我重新学习了 swift ，是的，重新学习，之前读过《swift 1.0 从入门到放弃》</p>\r\n\r\n<p>想起当年学习前端开发的经历，我开始写一些实际的小 app 来练手，总算是没有又一次『从入门到放弃』</p>\r\n\r\n<p>有一天突然想到，每一天不管开心还是难过，不管是平淡还是难忘，都要过的有意义&mdash;&mdash;从来不写日记的我第一次想每天花几分钟记录一下自己的一天了。（日记？上个世纪的东西了吧？！）</p>\r\n\r\n<p>于是就尝试了各种 app，记事类的、日记类的。却发现每个 app 都想做的很完美，带上各种功能，需要的不需要的全都有，干扰的东西太多。。。</p>\r\n\r\n<p>我只想一个单纯的记录自己每一天的 app，几张图片，一段文字，最多再加上一个地理位置，嗯，真实的自己，只有自己能看到，也不用担心有没有人点赞。</p>\r\n\r\n<p>干脆就自己用 swift 写一个吧：One Day.</p>\r\n\r\n<p>有一天正在听最喜欢的《One Day》，嗯，海贼王中艾斯被行刑的那一段的主题曲，这段剧情也是海贼王中给我感受最深的一段。于是 App 的名字就叫 One Day 了。</p>\r\n\r\n<p>One Day 的目标就只有一个：记录『最真实的自己』。所以从一开始就完全抛弃了社交和分享的功能。</p>\r\n', '0', '2017-07-01 17:29:18');
-INSERT INTO `posts` VALUES ('2', '1', 'weui.js发布', 'weui.js 是 WeUI 的轻量级 js 封装。 嗯，没啥好介绍的，直接复制项目的 Readme.md 过来好了，欢迎去项目的 Github 项目主页 star 并贡献代码。 概述 注意：由于微信小程序不支持dom操作，所以weui.js并不适用于小程序。不过WeUI也为小程', '<p>这几年，前端技术发展很快，每年都有新的技术名词流行，ReactJS、Angular 2、VUE、微信小程序。。。而我重新学习了 swift ，是的，重新学习，之前读过《swift 1.0 从入门到放弃》</p>\r\n\r\n<p>想起当年学习前端开发的经历，我开始写一些实际的小 app 来练手，总算是没有又一次『从入门到放弃』</p>\r\n\r\n<p>有一天突然想到，每一天不管开心还是难过，不管是平淡还是难忘，都要过的有意义&mdash;&mdash;从来不写日记的我第一次想每天花几分钟记录一下自己的一天了。（日记？上个世纪的东西了吧？！）</p>\r\n\r\n<p>于是就尝试了各种 app，记事类的、日记类的。却发现每个 app 都想做的很完美，带上各种功能，需要的不需要的全都有，干扰的东西太多。。。</p>\r\n\r\n<p>我只想一个单纯的记录自己每一天的 app，几张图片，一段文字，最多再加上一个地理位置，嗯，真实的自己，只有自己能看到，也不用担心有没有人点赞。</p>\r\n\r\n<p>干脆就自己用 swift 写一个吧：One Day.</p>\r\n\r\n<p>有一天正在听最喜欢的《One Day》，嗯，海贼王中艾斯被行刑的那一段的主题曲，这段剧情也是海贼王中给我感受最深的一段。于是 App 的名字就叫 One Day 了。</p>\r\n\r\n<p>One Day 的目标就只有一个：记录『最真实的自己』。所以从一开始就完全抛弃了社交和分享的功能。</p>\r\n', '0', '2017-07-01 17:31:02');
-INSERT INTO `posts` VALUES ('3', '1', '中国第三届CSS开发者大会即将召开', '中国第三届CSS开发者大会将于 2016 年 12 月 17 日在广州召开，这次请来的分享嘉宾还是非常牛逼的，有不少国内外的大牛，也有我的几个同事。 感兴趣可以直接访问上面的链接了解动态和详情。 部分讲师简介 还没有勾三股四的议题，看来是要压轴啊，好期待！', '<p>这几年，前端技术发展很快，每年都有新的技术名词流行，ReactJS、Angular 2、VUE、微信小程序。。。而我重新学习了 swift ，是的，重新学习，之前读过《swift 1.0 从入门到放弃》</p>\r\n\r\n<p>想起当年学习前端开发的经历，我开始写一些实际的小 app 来练手，总算是没有又一次『从入门到放弃』</p>\r\n\r\n<p>有一天突然想到，每一天不管开心还是难过，不管是平淡还是难忘，都要过的有意义&mdash;&mdash;从来不写日记的我第一次想每天花几分钟记录一下自己的一天了。（日记？上个世纪的东西了吧？！）</p>\r\n\r\n<p>于是就尝试了各种 app，记事类的、日记类的。却发现每个 app 都想做的很完美，带上各种功能，需要的不需要的全都有，干扰的东西太多。。。</p>\r\n\r\n<p>我只想一个单纯的记录自己每一天的 app，几张图片，一段文字，最多再加上一个地理位置，嗯，真实的自己，只有自己能看到，也不用担心有没有人点赞。</p>\r\n\r\n<p>干脆就自己用 swift 写一个吧：One Day.</p>\r\n\r\n<p>有一天正在听最喜欢的《One Day》，嗯，海贼王中艾斯被行刑的那一段的主题曲，这段剧情也是海贼王中给我感受最深的一段。于是 App 的名字就叫 One Day 了。</p>\r\n\r\n<p>One Day 的目标就只有一个：记录『最真实的自己』。所以从一开始就完全抛弃了社交和分享的功能。</p>\r\n', '1', '2017-07-01 17:31:37');
-INSERT INTO `posts` VALUES ('4', '1', 'weui.js发布', 'weui.js 是 WeUI 的轻量级 js 封装。 嗯，没啥好介绍的，直接复制项目的 Readme.md 过来好了，欢迎去项目的 Github 项目主页 star 并贡献代码。 概述 注意：由于微信小程序不支持dom操作，所以weui.js并不适用于小程序。不过WeUI也为小程', '', '0', '2017-07-01 17:32:08');
-INSERT INTO `posts` VALUES ('5', '1', '中国第三届CSS开发者大会即将召开', '中国第三届CSS开发者大会将于 2016 年 12 月 17 日在广州召开，这次请来的分享嘉宾还是非常牛逼的，有不少国内外的大牛，也有我的几个同事。 感兴趣可以直接访问上面的链接了解动态和详情。 部分讲师简介 还没有勾三股四的议题，看来是要压轴啊，好期待！', '', '0', '2017-07-01 17:32:27');
-INSERT INTO `posts` VALUES ('6', '1', 'JS the Hardcore: 执行上下文(Execution Context)', '先看个例子: console.log(a) console.log(foo()) var a = \'hello world\' function foo() { console.log(\'foo\') } 估计大部分人早就对这种问题了如指掌了，输出结果也是脱口而出', '', '0', '2017-07-01 17:32:49');
-INSERT INTO `posts` VALUES ('7', '1', '中国第三届CSS开发者大会即将召开', 'weui.js 是 WeUI 的轻量级 js 封装。 嗯，没啥好介绍的，直接复制项目的 Readme.md 过来好了，欢迎去项目的 Github 项目主页 star 并贡献代码。 概述 注意：由于微信小程序不支持dom操作，所以weui.js并不适用于小程序。不过WeUI也为小程', '', '1', '2017-07-01 17:33:05');
-INSERT INTO `posts` VALUES ('8', '1', 'JS the Hardcore: 执行上下文(Execution Context)', '中国第三届CSS开发者大会将于 2016 年 12 月 17 日在广州召开，这次请来的分享嘉宾还是非常牛逼的，有不少国内外的大牛，也有我的几个同事。 感兴趣可以直接访问上面的链接了解动态和详情。 部分讲师简介 还没有勾三股四的议题，看来是要压轴啊，好期待！', '', '0', '2017-07-01 17:33:24');
-INSERT INTO `posts` VALUES ('9', '1', '中国第三届CSS开发者大会即将召开', 'weui.js 是 WeUI 的轻量级 js 封装。 嗯，没啥好介绍的，直接复制项目的 Readme.md 过来好了，欢迎去项目的 Github 项目主页 star 并贡献代码。 概述 注意：由于微信小程序不支持dom操作，所以weui.js并不适用于小程序。不过WeUI也为小程', '', '0', '2017-07-01 17:34:21');
-INSERT INTO `posts` VALUES ('10', '1', 'weui.js发布', '中国第三届CSS开发者大会将于 2016 年 12 月 17 日在广州召开，这次请来的分享嘉宾还是非常牛逼的，有不少国内外的大牛，也有我的几个同事。 感兴趣可以直接访问上面的链接了解动态和详情。 部分讲师简介 还没有勾三股四的议题，看来是要压轴啊，好期待！', '', '0', '2017-07-01 17:34:07');
-
--- ----------------------------
--- Table structure for `users`
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `pass` char(32) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `avatar` varchar(255) DEFAULT NULL,
-  `gender` tinyint(4) DEFAULT NULL,
-  `phone` char(11) DEFAULT NULL,
-  `company` varchar(255) DEFAULT NULL,
-  `homepage` varchar(255) DEFAULT NULL,
-  `alt` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', '小明', 'e10adc3949ba59abbe56e057f20f883e', 'xiaoming@itcast.cn', 'uploads\\avatar\\avatar-1498880853834.jpg', '0', '12312345678', '传智播客', 'http://www.itcast.cn', '一名程序员');
-INSERT INTO `users` VALUES ('2', '小米', 'e10adc3949ba59abbe56e057f20f883e', 'xiaomi@itcast.cn', null, '0', '12312345678', '传智播客', 'http://www.itcast.cn', '一名程序员');
